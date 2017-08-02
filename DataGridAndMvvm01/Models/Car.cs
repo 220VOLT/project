afdb170d5344205c88c535c7f7fc306f18be0fdb
220VOLT <220VOLT_vip@mail.ru>
2017-08-02 23:14:46 +0700

    first commit

---
+++ DataGridAndMvvm01/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataGridAndMvvm01.Models
+{
+        public class Car : INotifyPropertyChanged
+        {
+            private int _price;
+            public string Name { get; set; }
+            //Свойство, которое будет уведомлять о своём изменении:
+            public int Price
+            {
+                get { return _price; }
+                set
+                {
+                    if (_price != value)
+                    {
+                        _price = value;
+                        OnPropertyChanged("Price");
+                    }
+                }
+            }
+            //Реализация интерфейса INotifyPropertyChanged
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+}
---
+++ DataGridAndMvvm01/ViewModel/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using DataGridAndMvvm01.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DataGridAndMvvm01.ViewModel
+{
+    class ViewModel
+    {
+        public ICommand ClickCommandEditSelectedObject { get; set; }
+        public int SelectedIndex { get; set; }
+        public ICommand ClickCommandEditFirstObject { get; set; }
+        public ObservableCollection<Car> Cars { get; set; }
+        public string NewName { get; set; }
+        public int NewPrice { get; set; }
+        public ICommand ClickCommand { get; set; }
+        public ViewModel()
+        {
+            ClickCommandEditSelectedObject = new RelayCommand(arg => ClickMethodEditSelectedObject());
+            //ClickCommandEditFirstObject = new RelayCommand(arg => ClickMethodEditFirstObject());
+            Cars = new ObservableCollection<Car>()
+            {
+                new Car {Name = "Honda", Price= 30000},
+                new Car {Name = "Ford", Price= 15000},
+                new Car {Name = "Lada", Price= 5000}
+            };
+            ClickCommand = new RelayCommand(arg => ClickMethod());
+        }
+
+        private void ClickMethod()
+        {
+            Cars.Add(new Car() { Name = NewName, Price = NewPrice });
+        }
+        private void ClickMethodEditFirstObject()
+        {
+            Cars.First().Price *= 2;
+        }
+   
+        //И создаём метод-обработчик:
+        private void ClickMethodEditSelectedObject()
+        {
+            if (SelectedIndex != -1)
+            {
+                Cars[SelectedIndex].Price *= 2;
+            }
+        }
+    }
+}
